d507cb368c8c7373991103cec3fd31fce5b00e60
its julii <nanji828@gmail.com>
2023-01-13 14:45:22 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Food_delivery
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double pileshkoMenu = double.Parse(Console.ReadLine()) * 10.35;
+            double ribaMenu = double.Parse(Console.ReadLine()) * 12.40;
+            double vegeMenu = double.Parse(Console.ReadLine()) * 8.15;
+
+            double totalMenus = (pileshkoMenu + ribaMenu + vegeMenu);
+            double totalWithDiscount = totalMenus * 0.20;
+            double totalPrice = totalMenus + totalWithDiscount + 2.50;
+
+            Console.WriteLine(totalPrice);
+        }
+    }
+}
